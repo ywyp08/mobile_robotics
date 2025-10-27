e06ea1ea5fcef5328afd67c6e36762a07f9359ca
ywyp08 <stepa.spilka@gmail.com>
2025-10-27 16:08:42 +0400

    logic change: follow right edge

--- lab5/main.py
+++ lab5/main.py
@@ -15,57 +15,23 @@ color_sensor = ColorSensor(Port.S3)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
 # Settings
-FORWARD_SPEED = 100     # mm/s
-WAIT_TIME = 100         # ms
-TURN_BASE = 10          # degrees
-ULTRA_THRESH = 50        # mm
+FORWARD_SPEED = 10     # mm/s
+TURN_RATE = 30          # deg/s for correcting turns
+ULTRA_THRESH = 50        # mm for obstacle
 
 # Functions
-def sees_black():
+def sensor_is_on_black():
     return color_sensor.color() == Color.BLACK
 
-def sees_grey():
-    r, g, b = color_sensor.rgb()
-    reflection = color_sensor.reflection()
-    rgb_range = (7 <= r <= 13) and (10 <= g <= 18) and (18 <= b <= 26)
-    reflection_range = (12 <= reflection <= 26)
-    ev3.screen.clear()
-    ev3.screen.print("R:", r, "G:", g, "B:", b)
-    ev3.screen.print("Ref:", reflection)
-    wait(5000)
-    return rgb_range and reflection_range
-
-def look_around(turn_angle):
-    robot.turn(turn_angle)
-    wait(WAIT_TIME)
-    if sees_black():
-        return
-    robot.turn(-1*turn_angle)
-    robot.turn(-1*turn_angle)
-    wait(WAIT_TIME)
-    if sees_black():
-        return
-    robot.turn(turn_angle)
-
 # Start
-grey_patch = 0
-run = True
-while run:
-    distance = ultra_sensor.distance()
-    if distance < ULTRA_THRESH:
+while True:
+    if ultra_sensor.distance() < ULTRA_THRESH:
         robot.stop()
         ev3.speaker.beep()
         while ultra_sensor.distance() < ULTRA_THRESH:
-            wait(WAIT_TIME)
-        wait(WAIT_TIME)
-    if sees_black():
-        robot.drive(FORWARD_SPEED, 0)
+            wait(10)
+    if sensor_is_on_black():
+        robot.drive(FORWARD_SPEED, +TURN_RATE)
     else:
-        robot.stop()
-        wait(WAIT_TIME)
-        turn_angle = TURN_BASE
-        while not sees_black():
-            look_around(turn_angle)
-            turn_angle += TURN_BASE
-    wait(WAIT_TIME)
-
+        robot.drive(0, -TURN_RATE)
+    wait(10)
