cb0ffc1e6a66ec241db8ff306401a7025b29675c
ywyp08 <stepa.spilka@gmail.com>
2025-10-30 12:48:48 +0400

    added logging

--- task4/main.py
+++ task4/main.py
@@ -5,6 +5,8 @@ from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
+from pybricks.tools import wait, StopWatch, DataLog
+import math
 
 # Setup
 ev3 = EV3Brick()
@@ -13,11 +15,17 @@ right_motor = Motor(Port.C)
 ultra_sensor = UltrasonicSensor(Port.S2)
 color_sensor = ColorSensor(Port.S3)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+watch = StopWatch()
+data = DataLog('time_ms', 'x_mm', 'y_mm', 'heading_deg', 'speed', 'turn', 'end', name='path_log', timestamp=False, extension='csv')
 
 # Settings
-FORWARD_SPEED = 10     # mm/s
-TURN_RATE = 30          # deg/s for correcting turns
+FORWARD_SPEED = 15     # mm/s
+TURN_RATE = 50          # deg/s for correcting turns
 ULTRA_THRESH = 50        # mm for obstacle
+
+# Variables
+x, y, heading = 0.0, 0.0, 0.0
+last_distance = 0.0
 
 # Functions
 def sensor_is_on_black():
@@ -25,13 +33,29 @@ def sensor_is_on_black():
 
 # Start
 while True:
+    end = 0
+    distance = robot.distance()
+    delta_d = distance - last_distance
+    last_distance = distance
+
+    heading = robot.angle()
+    rad = math.radians(heading)
+    x += delta_d * math.cos(rad)
+    y += delta_d * math.sin(rad)
+
     if ultra_sensor.distance() < ULTRA_THRESH:
         robot.stop()
         ev3.speaker.beep()
         while ultra_sensor.distance() < ULTRA_THRESH:
             wait(10)
     if sensor_is_on_black():
-        robot.drive(FORWARD_SPEED, +TURN_RATE)
+        speed = FORWARD_SPEED
+        turn = +TURN_RATE
     else:
-        robot.drive(FORWARD_SPEED, -TURN_RATE)
+        speed = FORWARD_SPEED
+        turn = -TURN_RATE
+
+    robot.drive(speed, turn)
+
+    data.log(watch.time(), x, y, heading, speed, turn, end)
     wait(10)
